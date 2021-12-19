0f427f33e0c8d6a005a02fa243b0457172bfa34d
Julian Cuni <julian.cuni@microservices.al>
2021-12-20 00:15:10 +0100

    Test Frontend CI CD caprover

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -18,37 +18,6 @@
 
     $: homepage = $t("home");
 
-    const name = "Julian Cuni";
-    const email = "juliancuni@gmail.com";
-    const username = "jc";
-    const password = "123456789Aa";
-
-    import {
-        login,
-        register,
-        logout,
-        getLoggedInUser,
-        requestPassRecovery,
-    } from "../js/services/parse/auth.service";
-
-    const loginUser = async () => {
-        const user = await login(username, password);
-    };
-
-    const registerUser = async () => {
-        const user = await register(name, email, username, password);
-    };
-
-    const logoutUser = async () => {
-        await logout();
-    };
-    const getUser = async () => {
-        await getLoggedInUser();
-    };
-
-    const resetPass = async () => {
-        await requestPassRecovery(email);
-    };
 </script>
 
 <Page name="home">
@@ -56,10 +25,4 @@
     <Block strong>
         <p>Home {homepage.njoftim}</p>
     </Block>
-
-    <Button onClick={loginUser}>Login</Button>
-    <Button onClick={registerUser}>Register</Button>
-    <Button onClick={logoutUser}>Logout</Button>
-    <Button onClick={getUser}>GetUser</Button>
-    <Button onClick={resetPass}>Reset Pass</Button>
 </Page>
